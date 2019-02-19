0011abc1e4391096bc0c779f44f2683a00a6d99f
feixiang7701 <feixiang7701@163.com>
2019-02-19 11:35:18 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
+"""
+This is just a test program,the Layer variable is only be initialized
+and its parameters is set freestyle. 
+TODO:
+[1] A real iamge segmentation project with ConvCRF.
+"""
+
+
 import tensorflow as tf
 tf.enable_eager_execution()
 
@@ -30,13 +38,7 @@ img,label_img=_process_pathnames(image_name,label_name)
 img=tf.expand_dims(img,axis=0)
 label_img=tf.expand_dims(label_img,axis=0)
 
-"""
-This is just a test program,the Layer variable is only be initialized
-and its parameters is set freestyle. 
-
-TODO:
-[1] A real iamge segmentation project with ConvCRF.
-"""
+#ConvCRF Test
 from ConvCrf_layer import ConvCRF
 image_dims=(1280,1918)
 CRF=ConvCRF(image_dims,7,4,2,160,10,10,10)
